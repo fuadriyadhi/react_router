dbe003a096cc2442cc4fab547f832001552dd44f
fuadriyadhi <fuadriyadhi678@gmail.com>
2022-10-04 22:27:57 +0700

    commit

---
+++ src/pages/About.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+import MainLayout from './../layouts/MainLayout';
+
+export default function About() {
+  return (
+    <MainLayout>
+      <h1>About Page</h1>
+      <img
+        src="https://images.pexels.com/photos/13811598/pexels-photo-13811598.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load"
+        alt="gambar"
+        style={{
+          width: 100,
+          height: 200,
+          objectFit: "cover",
+        }}
+      />
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Ea in delectus voluptas optio accusamus, animi omnis porro nesciunt assumenda officia necessitatibus perspiciatis harum quasi, voluptatem, accusantium quis aliquid! Molestias,
+        fugit?
+      </p>
+    </MainLayout>
+  );
+}
---
+++ src/pages/Home.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import MainLayout from './../layouts/MainLayout';
+
+export default function Home() {
+  return (
+    <MainLayout>
+      <h1>Home Page</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Ea in delectus voluptas optio accusamus, animi omnis porro nesciunt assumenda officia necessitatibus perspiciatis harum quasi, voluptatem, accusantium quis aliquid! Molestias,
+        fugit?
+      </p>
+    </MainLayout>
+  );
+}
